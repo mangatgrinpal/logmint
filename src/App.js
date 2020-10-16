f1c78e94b97f37f904b4c6a44e2883107bf23cd1
Ginny Mangat <mangatgrinpal@gmail.com>
2020-10-15 20:54:54 -0700

    changed layout, ditching Auth0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import './App.css';
+import SignInForm from './components/SignInForm';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
 
 function App() {
+
   return (
-    <div>
-    </div>
+  	<Container fluid={true}>
+  		<Row>
+  			<Col>
+  				<h1>Welcome to Logmint</h1>
+  			</Col>
+  		</Row>
+  		<Row>
+  			<Col xs={6} md={3}>
+  				<SignInForm />
+  			</Col>
+  		</Row>
+  		<Row className='pt-5'>
+  			<Button>Google</Button>
+  			<Button>Microsoft</Button>
+  		</Row>
+  	</Container>
   );
 }
 
---
+++ src/components/SignInForm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+const SignInForm = () => {
+	return (
+		<Form>
+			<Form.Group>
+				<Form.Label>Email</Form.Label>
+				<Form.Control 
+					type='text'
+					name='email'
+					placeholder='email'/>
+
+			</Form.Group>
+			<Form.Group>
+				<Form.Label>Password</Form.Label>
+				<Form.Control
+					type='password'
+					placeholder='password'/>
+			</Form.Group>
+			<Button>
+				Submit
+			</Button>
+		</Form>
+	)
+}
+
+export default SignInForm
