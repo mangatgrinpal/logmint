729bb549f825d518c9f1264fb019406cad09c510
Ginny Mangat <mangatgrinpal@gmail.com>
2020-10-20 13:01:55 -0700

    added various functions to logs and users pages to allow user/log creation

--- src/components/LogsPage.js
+++ src/components/LogsPage.js
@@ -4,6 +4,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -11,9 +12,27 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const LogsPage = () => {
 
 	const [ showModal, setShowModal ] = useState(false);
+	const [ logs, setLogs ] = useState([])
+	const [ logInfo, setLogInfo ] = useState({
+		logName: ''
+	})
 	
 	const handleShow = () => setShowModal(true)
 	const handleClose = () => setShowModal(false)
+
+	const handleInputChange = e => {
+		setLogInfo({...logInfo, [e.target.name]: e.target.value})
+	}
+
+	const handleSubmit = e => {
+		e.preventDefault()
+		const newLogs = Object.assign([])
+		newLogs.push(logInfo)
+		setLogs(newLogs)
+
+	}
+
+	const { logName } = logInfo;
 
 	return (
 		<>
@@ -33,33 +52,63 @@ const LogsPage = () => {
 				</Col>
 			</Row>
 			<Row className='bg-light h-100 pt-5'>
-				<Col className='text-center pt-3'>
-					<FontAwesomeIcon
-						size='3x'
-						icon={['fas', 'cloud-upload-alt']} />
-					<h3>You haven't added any logs yet.</h3>
-					<p>Add a CI/CD to begin tracking now</p>
-					<Button onClick={handleShow}>
-						<FontAwesomeIcon
-							icon={['fas', 'clipboard-list']} />
-						&nbsp;
-						Add CI/CD
-					</Button>
-				</Col>
+				{
+					logs.length === 0 ? (
+						<Col className='text-center pt-3'>
+							<FontAwesomeIcon
+								size='3x'
+								icon={['fas', 'cloud-upload-alt']} />
+							<h3>You haven't added any logs yet.</h3>
+							<p>Add a CI/CD to begin tracking now</p>
+							<Button onClick={handleShow}>
+								<FontAwesomeIcon
+									icon={['fas', 'clipboard-list']} />
+								&nbsp;
+								Add CI/CD
+							</Button>
+						</Col>
+					) : (
+
+							logs.map((log, index)=> {
+								return (
+									<Row key={index}>
+										<Col>
+											{log.logName}
+										</Col>
+									</Row>
+								)
+							})
+
+					)
+				}
+				
 			</Row>
 			<Modal show={showModal} onHide={handleClose} centered>
 				<Modal.Header closeButton>
-					<Modal.Title>Modal heading</Modal.Title>
+					<Modal.Title>Add CI/CD</Modal.Title>
 				</Modal.Header>
-				<Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-				<Modal.Footer>
-					<Button variant="secondary" onClick={handleClose}>
-						Close
-          </Button>
-					<Button variant="primary" onClick={handleClose}>
-						Save Changes
-          </Button>
-				</Modal.Footer>
+				<Form onSubmit={handleSubmit}>
+					<Modal.Body>
+						<Form.Row>
+							<Form.Group as={Col}>
+								<Form.Label>Name</Form.Label>
+								<Form.Control 
+									name='logName'
+									type='text'
+									onChange={handleInputChange}
+									value={logName}/>
+							</Form.Group>
+						</Form.Row>
+					</Modal.Body>
+					<Modal.Footer>
+						<Button variant="secondary" onClick={handleClose}>
+							Close
+						</Button>
+						<Button variant="primary" type='submit' onClick={handleClose}>
+							Create CI/CD
+						</Button>
+					</Modal.Footer>
+				</Form>
 			</Modal>
 		</>
 	)
--- src/components/UsersPage.js
+++ src/components/UsersPage.js
@@ -4,15 +4,34 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const UsersPage = () => {
 
 	const [ showModal , setShowModal] = useState(false);
+	const [ users, setUsers ] = useState([]);
+	const [ userInfo, setUserInfo ] = useState({
+		email: ''
+	})
 
 	const handleShow = () => setShowModal(true);
 	const handleClose = () => setShowModal(false);
+
+	const handleInputChange = e => {
+		setUserInfo({...userInfo, [e.target.name]: e.target.value})
+	}
+
+	const handleSubmit = e => {
+		e.preventDefault()
+		const newUsers = Object.assign([])
+		newUsers.push(userInfo)
+		setUsers(newUsers)
+
+	}
+
+	const { email } = userInfo;
 
 	return (
 		<>
@@ -32,33 +51,62 @@ const UsersPage = () => {
 				</Col>
 			</Row>
 			<Row className='bg-light h-100 pt-5'>
-				<Col className='text-center pt-3'>
-					<FontAwesomeIcon
-						size='3x'
-						icon={['fas', 'cloud-upload-alt']} />
-					<h3>You don't have any users added yet.</h3>
-					<p>Add users to begin tracking now</p>
-					<Button onClick={handleShow}>
-						<FontAwesomeIcon
-							icon={['fas', 'user']} />
-						&nbsp;
-						Add User
-					</Button>
-				</Col>
+				{
+					users.length === 0 ? (
+						<Col className='text-center pt-3'>
+							<FontAwesomeIcon
+								size='3x'
+								icon={['fas', 'cloud-upload-alt']} />
+							<h3>You don't have any users added yet.</h3>
+							<p>Add users to begin tracking now</p>
+							<Button onClick={handleShow}>
+								<FontAwesomeIcon
+									icon={['fas', 'user']} />
+								&nbsp;
+								Add User
+							</Button>
+						</Col>
+					) : (
+						
+						users.map((user, index)=> {
+							return (
+								<Row key={index}>
+									<Col>
+										{user.email}
+									</Col>
+								</Row>
+							)
+						})
+					)
+				}
+				
 			</Row>
 			<Modal show={showModal} onHide={handleClose} centered>
 				<Modal.Header closeButton>
 					<Modal.Title>Modal heading</Modal.Title>
 				</Modal.Header>
-				<Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-				<Modal.Footer>
-					<Button variant="secondary" onClick={handleClose}>
-						Close
-          </Button>
-					<Button variant="primary" onClick={handleClose}>
-						Save Changes
-          </Button>
-				</Modal.Footer>
+				<Form onSubmit={handleSubmit}>
+					<Modal.Body>
+						<Form.Row>
+							<Form.Group as={Col}>
+								<Form.Label>Name</Form.Label>
+								<Form.Control
+									name='email'
+									type='text'
+									onChange={handleInputChange}
+									value={email} />
+							</Form.Group>
+						</Form.Row>
+					</Modal.Body>
+					<Modal.Footer>
+						<Button variant="secondary" onClick={handleClose}>
+							Close
+						</Button>
+						<Button variant="primary" type='submit' onClick={handleClose}>
+							Create CI/CD
+						</Button>
+					</Modal.Footer>
+				</Form>
 			</Modal>
 		</>
 		
